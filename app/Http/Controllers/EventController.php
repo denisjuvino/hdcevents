b47d6251aefa6995072f650ad958bcdbda77fece
Denis Juvino Bento <contatodenisjuvino@gmail.com>
2021-02-22 15:46:54 -0300

    Criar eventos/mais modificações

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -29,7 +29,7 @@ class EventController extends Controller
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
 
     }
 
